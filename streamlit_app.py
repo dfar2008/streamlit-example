b1af33672b1e4c65dcd5f81dc983f7fa1c2b3176
dfar2008 <dfar2008@gmail.com>
2023-06-13 16:56:43 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,6 @@ import numpy as np
 chart_data = pd.DataFrame(
     np.random.randn(20, 3),
     columns=['a', 'b', 'c'])
+df = pd.read_csv('https://testnewoky.crm123.cn/8d862b2a-9b63-4b9a-b375-10d4138f130a.csv')
 
-st.line_chart(chart_data)
+st.line_chart(df)
